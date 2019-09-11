1a95bac7a2b88a214d6348a26b99609b106aa683
Raphael Carvalho <ph_rapha5@hotmail.com>
2019-09-12 00:28:07 +0100

    Project created

---
+++ src/main/java/com/spaceph/workshopjpa/WorkshopjpaApplication.java
@@ -0,0 +1,13 @@
+package com.spaceph.workshopjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopjpaApplication.class, args);
+	}
+
+}
