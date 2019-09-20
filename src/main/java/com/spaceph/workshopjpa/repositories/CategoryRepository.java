db9241c55ecd93465f52b05d18e7d7d4264aed8a
Raphael Carvalho <ph_rapha5@hotmail.com>
2019-09-20 19:20:17 +0100

    Category

---
+++ src/main/java/com/spaceph/workshopjpa/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.spaceph.workshopjpa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.spaceph.workshopjpa.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
